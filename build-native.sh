aa571ee1841431d165d833d71f5d8c46623f8ed6
Alex Wood <alexljwood24@hotmail.co.uk>
2020-04-23 13:12:26 +0100

    Update destination of native image

--- build-native.sh
+++ build-native.sh
@@ -1,2 +1,2 @@
 ./gradlew clean shadowJar
-native-image --report-unsupported-elements-at-runtime -jar build/libs/Switcher.jar switcher --no-server
+native-image --report-unsupported-elements-at-runtime -jar build/libs/Switcher.jar build/libs/switcher --no-server
