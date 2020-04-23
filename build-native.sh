9af551f6580b444fcdd45ccda52b85bb7121a917
Alex Wood <alexljwood24@hotmail.co.uk>
2020-04-23 13:21:41 +0100

    Fix build-native.sh using the wrong file name when building a native image

--- build-native.sh
+++ build-native.sh
@@ -1,2 +1,2 @@
 ./gradlew clean shadowJar
-native-image --report-unsupported-elements-at-runtime -jar build/libs/Switcher.jar build/libs/switcher --no-server
+native-image --report-unsupported-elements-at-runtime -jar build/libs/switcher.jar build/libs/switcher --no-server
