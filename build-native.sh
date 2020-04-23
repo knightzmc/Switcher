e5b743aa771e092de276a198dce9be1d6d631853
Alex Wood <alexljwood24@hotmail.co.uk>
2020-04-23 13:01:42 +0100

    Change project name to lowercase

--- build-native.sh
+++ build-native.sh
@@ -1,2 +1,2 @@
 ./gradlew clean shadowJar
-native-image --report-unsupported-elements-at-runtime -jar build/libs/Switcher.jar Switcher --no-server
+native-image --report-unsupported-elements-at-runtime -jar build/libs/Switcher.jar switcher --no-server
